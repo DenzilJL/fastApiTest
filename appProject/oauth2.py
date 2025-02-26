d1ba9da706d93eed3383de327aa46d66a12c9bad
DenzilLobo93 <denzillobo145@gmail.com>
2025-02-26 19:13:37 +0530

    verifying jwt token, usage of create jwt token

--- appProject/oauth2.py
+++ appProject/oauth2.py
@@ -1,5 +1,8 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schema
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 # secret key
 # algorithm
 # expire time
@@ -7,10 +10,31 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     token = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
     return token
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+
+        id: str = payload.get("user_id")
+        if id is None:
+            return credentials_exception
+        token_data = schema.TokenData(id=id)
+        return token_data
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"crendials mismatched", headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
