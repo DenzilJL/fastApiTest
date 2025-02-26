aec9caaa6ead9ac1e6a61022984ec63686383a4e
DenzilLobo93 <denzillobo145@gmail.com>
2025-02-26 20:08:33 +0530

    foreign key seeting

--- appProject/schema.py
+++ appProject/schema.py
@@ -3,20 +3,6 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 
-class Post (BaseModel):
-    title: str
-    description: str
-    published: bool = True
-
-
-class PostResp (Post):
-    id: int
-    created_at: datetime
-
-    class Config:
-        orm = True
-
-
 class UserBase (BaseModel):
     user_name: str
     email: EmailStr
@@ -48,3 +34,19 @@ class Token (BaseModel):
 
 class TokenData (BaseModel):
     id: int
+
+
+class Post (BaseModel):
+    title: str
+    description: str
+    published: bool = True
+    owner_id: int
+
+
+class PostResp (Post):
+    id: int
+    created_at: datetime
+    owner: UserBase
+
+    class Config:
+        orm = True
