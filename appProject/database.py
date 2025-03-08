7f5f59d9e7662145db97b15f4a038936dfca169b
DenzilLobo93 <denzillobo145@gmail.com>
2025-03-08 09:12:51 +0530

    create a config file to store important env values

--- appProject/database.py
+++ appProject/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from .config import DATABASE_URL
 # SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ipAddress/Host>/DatabaseName'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/sqlAlcemy'
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- appProject/oauth2.py
+++ appProject/oauth2.py
@@ -3,12 +3,13 @@ from datetime import datetime, timedelta
 from . import schema
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from .config import OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES, OAUTH_ALGORITHM, OAUTH_SECRET_KEY
 # secret key
 # algorithm
 # expire time
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = OAUTH_SECRET_KEY
+ALGORITHM = OAUTH_ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
